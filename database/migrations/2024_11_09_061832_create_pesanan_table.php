c30041c143817473ae8e597e3c77c024bab4482b
evanel15 <evanelmaska10@gmail.com>
2024-11-09 13:33:58 +0700

    database

---
+++ database/migrations/2024_11_09_061832_create_pesanan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_customer');
+            $table->integer('berat');
+            $table->date('tanggal_masuk');
+            $table->string('harga_total');
+            $table->enum('status', ['waiting list', 'diproses', 'selesai', 'diambil', 'dibatalkan']);
+            $table->date('tanggal_diambil')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanan');
+    }
+};

490643ac9eac7784e00b67cc312b463b08ce7284
ArkaaKai <manrayxd697@gmail.com>
2024-12-11 17:20:17 +0700

    detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,17 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
 
-@foreach ($data as $laundry)
-    <div>
-        <img src="{{asset('storage/images/pesanan' .$laundry->image) }}" style="width: 100px;" alt="">
+@section('content')
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th>Nama pemesan</th>
+                                <td>{{$data->nama_customer  }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img src="{{asset('storage/images/pesanan/'.$data->image)}}" class="img-thumbnail" alt="">
+                </div>
+            </div>
+        </div>
     </div>
-@endforeach
+    <form action="{{route('laundry')}}">
+        <div class="btn btn-danger">
+                Back
+            </div>
+    </form>
 
-</body>
-</html>
+</div>
+
+
+@endsection

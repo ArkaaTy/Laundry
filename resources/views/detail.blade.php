17610b5f8e8e5cabc4fe4a713eb26f72079bf1d2
ArkaaKai <manrayxd697@gmail.com>
2024-12-12 11:44:50 +0700

    dah tu eko

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,30 +6,55 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+        <div class="card mb-3">
+                <div class="card-body">
+                    <img src="{{asset('storage/images/pesanan/'.$data->image)}}" class="img-fluid" alt="">
+                </div>
+            </div>
             <div class="card mb-4">
+
                 <div class="card-body">
                     <table class="table table-hover">
+
                         <tbody>
                             <tr>
-                                <th>Nama pemesan</th>
-                                <td>{{$data->nama_customer  }}</td>
+                                <th>
+                                    Nama pemesan
+                                </th>
+                                <td>{{$data->nama_customer}}</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Berat
+                                </th>
+                                <td>{{$data->berat}}.Kg</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Tanggal Masukk
+                                </th>
+                                <td>{{$data->tanggal_masuk}}</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Harga
+                                </th>
+                                <td>Rp.{{ number_format($data->harga_total) }}</td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Kondisi
+                                </th>
+                                <td>{{$data->status}}</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <img src="{{asset('storage/images/pesanan/'.$data->image)}}" class="img-thumbnail" alt="">
-                </div>
-            </div>
+
         </div>
     </div>
-    <form action="{{route('laundry')}}">
-        <div class="btn btn-danger">
-                Back
-            </div>
-    </form>
+    <a href="{{route('laundry')}}" class="btn btn-danger">Back</a>
 
 </div>
 
